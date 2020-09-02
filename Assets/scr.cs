c4672db6582026b3fb1c7d9e1b317201e44eb8a6
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2020-09-02 19:49:38 +0200

    Music and movement

---
+++ Assets/scr.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class scr : MonoBehaviour
+{
+    void Update()
+    {
+        Quit();
+    }
+
+    void Quit()
+    {
+        if (Keyboard.current.escapeKey.isPressed)
+        {
+            Application.Quit();
+        }
+    }
+}
