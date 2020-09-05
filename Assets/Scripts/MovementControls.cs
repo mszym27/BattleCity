b200d1c3aef1c8e0ff6046df9c1b6e2326d676a9
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2020-09-05 19:48:29 +0200

    SOLVED: log

--- Assets/Scripts/MovementControls.cs
+++ Assets/Scripts/MovementControls.cs
@@ -9,6 +9,8 @@ public class MovementControls : MonoBehaviour
     public InputAction input;
     public CharacterController controller;
 
+    private int previousRotation = 0;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -51,12 +53,82 @@ public class MovementControls : MonoBehaviour
         finalVector.z = inputVector.y;
 
         controller.Move(finalVector * Time.deltaTime * playerMovementSpeed);
-        //this.transform.Rotate(finalVector * 100);
 
-        Vector3 rotation = new Vector3(finalVector.z * playerTurnSpeed, inputVector.x * playerTurnSpeed, 0);
+        ////Debug.Log(finalVector.z); // góra 1, dół -1
+        ////Debug.Log(finalVector.x);// prawo 1,  lewo -1
 
-        this.transform.Rotate(rotation);
+        //// chcę tylko rotacji w osi y (x), ale o określony kąt, który wykryję dzięki zmianie w z
 
+        ////Vector3 rotation = new Vector3(0, inputVector.x * playerTurnSpeed, 0);
+
+        /*
+góra: 0
+dół: 3.14
+lewo: -1.60
+prawo: 1.60
+         */
+
+
+        Quaternion q = this.transform.rotation;
+        Vector3 v = q.ToEulerAngles();
+        //Debug.Log(v.y);
+        float currentRotation = v.y;
+
+        // left
+        if (finalVector.x == -1)
+        {
+            Vector3 rotation = new Vector3(0, -1.6f - currentRotation, 0);
+            
+            Debug.Log("left");
+            Debug.Log("TargetRotation" + rotation);
+
+            this.transform.Rotate(rotation);
+        }
+        // right
+        if (finalVector.x == 1)
+        {
+            Vector3 rotation = new Vector3(0, 1.6f - currentRotation, 0);
+
+            Debug.Log("right");
+            Debug.Log("TargetRotation" + rotation);
+
+            this.transform.Rotate(rotation);
+        }
+        // down
+        if (finalVector.z == -1)
+        {
+            Vector3 rotation = new Vector3(0, 90.0f - currentRotation, 0);
+
+            Debug.Log("down");
+            Debug.Log("TargetRotation" + rotation);
+
+            this.transform.Rotate(rotation);
+        }
+        // up
+        if (finalVector.z == 1)
+        {
+            Vector3 rotation = new Vector3(0, -1.0f * currentRotation, 0);
+
+            Debug.Log("up");
+            Debug.Log("TargetRotation" + rotation);
+
+            this.transform.Rotate(rotation);
+        }
+
+        ////Debug.Log(previousRotation + ", " + currentRotation);// prawo 1,  lewo -1
+
+        //Debug.Log(this.transform.rotation);
+
+        //previousRotation = currentRotation;
+
+        ////// Rotate the cube by converting the angles into a quaternion.
+        ////Quaternion target = Quaternion.Euler(0, finalVector.x * 1000, finalVector.z * 1000);
+
+        ////this.transform.rotation = Quaternion.Slerp(transform.rotation, target, 10000);
+
+        //float rotationStep = rotationSpeed;
+
+        //Vector3 directionToTarget = targetWaypoint.position - transform.position;
 
         //Vector2 inputVector = controls.ReadValue<Vector2>();
         //Vector3 finalVector = new Vector3();
