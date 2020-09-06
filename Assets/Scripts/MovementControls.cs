f7c6ca10b472bf7998a3d411656a2f63ad57cbfa
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2020-09-06 17:16:57 +0200

    Shooting + moving solved

--- Assets/Scripts/MovementControls.cs
+++ Assets/Scripts/MovementControls.cs
@@ -51,42 +51,44 @@ public class MovementControls : MonoBehaviour
         {
             shoot();
         }
-
-        Vector2 inputVector = moveInput.ReadValue<Vector2>();
-        Vector3 finalVector = new Vector3();
-
-        finalVector.x = inputVector.x;
-        finalVector.z = inputVector.y;
-
-        controller.Move(finalVector * Time.deltaTime * playerMovementSpeed);
-
-        // left
-        if (finalVector.x == -1)
+        else
         {
-            var target = new Quaternion(0, -0.7f, 0, 0.7f);
+            Vector2 inputVector = moveInput.ReadValue<Vector2>();
+            Vector3 finalVector = new Vector3();
 
-            this.transform.rotation = target;
-        }
-        // right
-        if (finalVector.x == 1)
-        {
-            var target = new Quaternion(0, 0.7f, 0, 0.7f);
+            finalVector.x = inputVector.x;
+            finalVector.z = inputVector.y;
 
-            this.transform.rotation = target;
-        }
-        // down
-        if (finalVector.z == -1)
-        {
-            var target = new Quaternion(0, -0.7f, 0, 0);
+            controller.Move(finalVector * Time.deltaTime * playerMovementSpeed);
 
-            this.transform.rotation = target;
-        }
-        // up
-        if (finalVector.z == 1)
-        {
-            var target = new Quaternion(0, 0, 0, 1);
+            // left
+            if (finalVector.x == -1)
+            {
+                var target = new Quaternion(0, -0.7f, 0, 0.7f);
 
-            this.transform.rotation = target;
+                this.transform.rotation = target;
+            }
+            // right
+            if (finalVector.x == 1)
+            {
+                var target = new Quaternion(0, 0.7f, 0, 0.7f);
+
+                this.transform.rotation = target;
+            }
+            // down
+            if (finalVector.z == -1)
+            {
+                var target = new Quaternion(0, -0.7f, 0, 0);
+
+                this.transform.rotation = target;
+            }
+            // up
+            if (finalVector.z == 1)
+            {
+                var target = new Quaternion(0, 0, 0, 1);
+
+                this.transform.rotation = target;
+            }
         }
     }
 
@@ -97,9 +99,6 @@ public class MovementControls : MonoBehaviour
             Instantiate(bullet, transform.position, this.transform.rotation);
 
             nextFireTime = Time.time + playerFireRate;
-
-            Debug.Log("Current time " + Time.time);
-            Debug.Log("Next time " + nextFireTime);
         }
     }
 }
